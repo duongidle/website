72c5ecc60a30ae754dda6400e3e6cc5053f115d0
duongidle <pervertcat2k2@gmail.com>
2022-10-01 00:52:02 +0700

    v0.1.2

--- app/__init__.py
+++ app/__init__.py
@@ -1,38 +1,83 @@
-from flask import Flask
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+from flask import Flask, request, current_app
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
+import email_validator
 
 db = SQLAlchemy()
-DB_NAME = "web_sys_local.db"
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = _l('Please log in to access this page.')
+mail = Mail()
+bootstrap = Bootstrap()
+moment = Moment()
+babel = Babel()
 
-def create_app():
+
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'this is secret key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config.from_object(config_class)
+
     db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+    mail.init_app(app)
+    bootstrap.init_app(app)
+    moment.init_app(app)
+    babel.init_app(app)
 
-    from .views import views
-    from .auth import auth
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/')
 
-    from .models import User, Note
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
 
-    create_database(app)
+    if not app.debug and not app.testing:
+        if app.config['MAIL_SERVER']:
+            auth = None
+            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+                auth = (app.config['MAIL_USERNAME'],
+                        app.config['MAIL_PASSWORD'])
+            secure = None
+            if app.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+                toaddrs=app.config['ADMINS'], subject='Myblog Failure',
+                credentials=auth, secure=secure)
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/myblog.log',
+                                           maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
 
-    @login_manager.user_loader
-    def load_user(id):
-        return models.User.query.get(int(id))
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Myblog startup')
 
     return app
 
-def create_database(app):
-    if not path.exists('authentication/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(current_app.config['LANGUAGES'])
+
+from app import models
---
+++ app/routes.py
@@ -0,0 +1,59 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for, Flask
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
+
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == "POST":
+        details = request.form
+        email = details['inputEmail']
+        password = details['inputPassword']
+
+        user = User.query.filter_by(user_email=email).first()
+        if user:
+            if check_password_hash(user.user_password, password):
+                flash('Dang nhap thanh cong!', category='success')
+                return render_template('index.html', user=current_user)
+            else:
+                flash('Mat khau khong dung!', category='error')
+        else:
+            flash('Nguoi dung khong ton tai!', category='error')
+            
+    return render_template('login.html', user=current_user)
+
+@auth.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == "POST":
+        details = request.form
+        name = details['inputName']
+        email = details['inputEmail']
+        password1 = details['inputPassword']
+        password2 = details['inputRepeatPassword']
+        phoneNumber = details['inputPhoneNumber']
+        
+        user = User.query.filter_by(user_email=email).first()
+        if user:
+            flash('Email da ton tai.', category='error')
+        elif len(email) < 4:
+            flash('Email khong chinh xac.', category='error')
+        elif len(name) < 2:
+            flash('Ten khong chinh xac.', category='error')
+        elif password1 != password2:
+            flash('Mat khau khong trung khop.', category='error')
+        elif len(password1) < 7:
+            flash('Mat khau khong hop le.', category='error')
+        elif len(phoneNumber) < 9:
+            flash('So dien thoai khong hop le.', category='error')
+        else:
+            new_user = User(user_email=email, user_name=name, user_password=generate_password_hash(password1, method='sha256'), phone_number = phoneNumber)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Tai khoan da duoc tao, vui long dang nhap lai!', category='success')
+            return render_template('login.html')
+    return render_template('register.html', user=current_user)
