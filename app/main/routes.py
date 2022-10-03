4c06a6a4e643b7e5284f7412d0439f3acb2dd551
duongidle <pervertcat2k2@gmail.com>
2022-10-03 12:54:19 +0700

    v0.1.5

--- app/main/routes.py
+++ app/main/routes.py
@@ -17,44 +17,27 @@ def before_request():
         db.session.commit()
     g.locale = str(get_locale())
 
-
 @bp.route('/', methods=['GET', 'POST'])
-@bp.route('/index', methods=['GET', 'POST'])
+@bp.route('/review', methods=['GET', 'POST'])
+def review():
+    return render_template('review/review.html')
+
+@bp.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('review/about.html')
+
+@bp.route('/term', methods=['GET', 'POST'])
+def term():
+    return render_template('review/term.html')
+
+@bp.route('/homepage', methods=['GET', 'POST'])
 @login_required
 def index():
     return render_template('index.html', title=_('Home'))
 
-
-@bp.route('/user/<username>')
+@bp.route('/dashboard', methods=['GET', 'POST'])
 @login_required
-def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    page = request.args.get('page', 1, type=int)
-    return render_template('user.html', user=user)
-
-
-@bp.route('/edit_profile', methods=['GET', 'POST'])
-@login_required
-def edit_profile():
-    form = EditProfileForm(current_user.username)
-    if form.validate_on_submit():
-        current_user.username = form.username.data
-        current_user.about_me = form.about_me.data
-        db.session.commit()
-        flash(_('Your changes have been saved.'))
-        return redirect(url_for('main.edit_profile'))
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.about_me.data = current_user.about_me
-    return render_template('edit_profile.html', title=_('Edit Profile'),
-                           form=form)
-
-
-@bp.route('/translate', methods=['POST'])
-@login_required
-def translate_text():
-    return jsonify({'text': translate(request.form['text'],
-                                      request.form['source_language'],
-                                      request.form['dest_language'])})
+def dashboard():
+    return render_template('dashboard.html')
 
 
